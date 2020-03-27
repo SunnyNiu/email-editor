c9a8a4d8744cd3ef7759f655ec32f0d31f15dfaa
Sunny Niu <sunnyyingli@gmail.com>
2020-03-27 22:23:19 +1300

    update server test case and fetch api test cases using jest.mock

---
+++ src/client/api/xmlFile.test.js
@@ -0,0 +1,34 @@
+import fetch from 'isomorphic-fetch';
+import { getXmlPath } from './xmlFile';
+
+jest.mock('isomorphic-fetch');
+
+describe('get files paths', () => {
+  it('GET /files/paths', () => {
+    fetch.mockReturnValueOnce(
+      Promise.resolve({
+        ok: true,
+        status: 200,
+        json: () => Promise.resolve({ paths: ['xml1', 'xml2'] }),
+      })
+    );
+    const expected = { paths: ['xml1', 'xml2'] };
+    return getXmlPath().then(json => {
+      expect(json).toEqual(expected);
+    });
+  });
+
+  it('GET /files/paths fail', () => {
+    fetch.mockReturnValueOnce(
+      Promise.resolve({
+        ok: false,
+        status: 504,
+        text: () => Promise.resolve('error'),
+      })
+    );
+    const expected = new Error(`Code: 504 Body: error`);
+    return getXmlPath().catch(e => {
+      expect(e).toEqual(expected);
+    });
+  });
+});
--- src/server/server.test.js
+++ src/server/server.test.js
@@ -1,12 +1,14 @@
 import request from 'supertest';
 import server from './server';
+import { readdir } from './util';
 
-jest.mock('./util', () => ({
-  readdir: (path, callback) => callback(undefined, ['xml1', 'xml2']),
-}));
+jest.mock('./util');
 
 describe('get files paths', () => {
   it('GET /files/paths', () => {
+    readdir.mockImplementation((path, callback) =>
+      callback(undefined, ['xml1', 'xml2'])
+    );
     const expected = '{"paths":["./src/xml/xml1","./src/xml/xml2"]}';
     return request(server)
       .get('/api/files/paths')
